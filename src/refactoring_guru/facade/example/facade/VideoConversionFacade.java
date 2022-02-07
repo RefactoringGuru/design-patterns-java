c2f03c2c2878be0a610039e2025f60656a0fb7a0
Alexander Shvets <neochief@shvetsgroup.com>
2022-02-07 17:39:40 +0200

    Content fixes.

--- src/refactoring_guru/facade/example/facade/VideoConversionFacade.java
+++ src/refactoring_guru/facade/example/facade/VideoConversionFacade.java
@@ -11,9 +11,9 @@ public class VideoConversionFacade {
         Codec sourceCodec = CodecFactory.extract(file);
         Codec destinationCodec;
         if (format.equals("mp4")) {
-            destinationCodec = new OggCompressionCodec();
-        } else {
             destinationCodec = new MPEG4CompressionCodec();
+        } else {
+            destinationCodec = new OggCompressionCodec();
         }
         VideoFile buffer = BitrateReader.read(file, sourceCodec);
         VideoFile intermediateResult = BitrateReader.convert(buffer, destinationCodec);
