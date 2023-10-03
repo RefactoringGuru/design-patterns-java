4a3db0a2f61bb3df1aaadcc3476a3ee008462b59
Kalin Ivanov <kalin2578@gmail.com>
2023-10-03 22:38:20 +0300

    minor update

--- src/refactoring_guru/builder/example/Demo.java
+++ src/refactoring_guru/builder/example/Demo.java
@@ -8,42 +8,30 @@ import refactoring_guru.builder.example.director.Director;
 
 /**
  * EN: Demo class. Everything comes together here.
- *
+ * EN: Director gets the concrete builder object from the client
+ * (application code). That's because the application knows better which
+ * builder to use to get a specific product.
+ * <p>
  * RU: Демо-класс. Здесь всё сводится воедино.
+ * RU: Директор получает объект конкретного строителя от клиента
+ * (приложения). Приложение само знает какой строитель использовать,
+ * чтобы получить нужный продукт.
  */
 public class Demo {
 
-    public static void main(String[] args) {
-        Director director = new Director();
+  public static void main(String[] args) {
+    Director director = new Director();
 
-        // EN: Director gets the concrete builder object from the client
-        // (application code). That's because application knows better which
-        // builder to use to get a specific product.
-        //
-        // RU: Директор получает объект конкретного строителя от клиента
-        // (приложения). Приложение само знает какой строитель использовать,
-        // чтобы получить нужный продукт.
-        CarBuilder builder = new CarBuilder();
-        director.constructSportsCar(builder);
+    CarBuilder builder = new CarBuilder();
+    director.constructSportsCar(builder);
 
-        // EN: The final product is often retrieved from a builder object, since
-        // Director is not aware and not dependent on concrete builders and
-        // products.
-        //
-        // RU: Готовый продукт возвращает строитель, так как Директор чаще всего
-        // не знает и не зависит от конкретных классов строителей и продуктов.
-        Car car = builder.getResult();
-        System.out.println("Car built:\n" + car.getCarType());
+    Car car = builder.getResult();
+    System.out.println("Car built:\n" + car.getCarType());
 
 
-        CarManualBuilder manualBuilder = new CarManualBuilder();
-
-        // EN: Director may know several building recipes.
-        //
-        // RU: Директор может знать больше одного рецепта строительства.
-        director.constructSportsCar(manualBuilder);
-        Manual carManual = manualBuilder.getResult();
-        System.out.println("\nCar manual built:\n" + carManual.print());
-    }
-
+    CarManualBuilder manualBuilder = new CarManualBuilder();
+    director.constructSportsCar(manualBuilder);
+    Manual carManual = manualBuilder.getResult();
+    System.out.println("\nCar manual built:\n" + carManual.print());
+  }
 }
