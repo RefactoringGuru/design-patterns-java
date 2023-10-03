29ad7b429d7db5107541c691e74ed956bcd7e856
Kalin Ivanov <kalin2578@gmail.com>
2023-10-03 13:47:17 +0300

    convert to a record

--- src/refactoring_guru/adapter/example/square/SquarePeg.java
+++ src/refactoring_guru/adapter/example/square/SquarePeg.java
@@ -3,25 +3,16 @@ package refactoring_guru.adapter.example.square;
 /**
  * EN: SquarePegs are not compatible with RoundHoles (they were implemented by
  * previous development team). But we have to integrate them into our program.
- *
+ * <p>
  * RU: КвадратныеКолышки несовместимы с КруглымиОтверстиями (они остались в
  * проекте после бывших разработчиков). Но мы должны как-то интегрировать их в
  * нашу систему.
  */
-public class SquarePeg {
-    private double width;
+public record SquarePeg(double width) {
 
-    public SquarePeg(double width) {
-        this.width = width;
-    }
-
-    public double getWidth() {
-        return width;
-    }
-
-    public double getSquare() {
-        double result;
-        result = Math.pow(this.width, 2);
-        return result;
-    }
+  public double getSquare() {
+    double result;
+    result = Math.pow(this.width, 2);
+    return result;
+  }
 }
