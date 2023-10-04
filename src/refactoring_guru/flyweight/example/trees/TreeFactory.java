fcfb5e11966455755002d80715c11667670aa595
Kalin Ivanov <kalin2578@gmail.com>
2023-10-04 16:25:05 +0300

    some changes in the flyweight package

--- src/refactoring_guru/flyweight/example/trees/TreeFactory.java
+++ src/refactoring_guru/flyweight/example/trees/TreeFactory.java
@@ -1,18 +1,16 @@
 package refactoring_guru.flyweight.example.trees;
 
-import java.awt.*;
 import java.util.HashMap;
 import java.util.Map;
 
-public class TreeFactory {
-    static Map<String, TreeType> treeTypes = new HashMap<>();
+public final class TreeFactory {
+  private static final Map<String, TreeType> treeTypes = new HashMap<>();
 
-    public static TreeType getTreeType(String name, Color color, String otherTreeData) {
-        TreeType result = treeTypes.get(name);
-        if (result == null) {
-            result = new TreeType(name, color, otherTreeData);
-            treeTypes.put(name, result);
-        }
-        return result;
-    }
+  private TreeFactory() throws InstantiationException {
+      throw new InstantiationException("Cannot create an instance of this class");
+  }
+
+  public static TreeType getTreeType(String name) {
+    return treeTypes.computeIfAbsent(name, k -> getTreeType(name));
+  }
 }
--- src/refactoring_guru/flyweight/example/trees/TreeType.java
+++ src/refactoring_guru/flyweight/example/trees/TreeType.java
@@ -3,20 +3,16 @@ package refactoring_guru.flyweight.example.trees;
 import java.awt.*;
 
 public class TreeType {
-    private String name;
-    private Color color;
-    private String otherTreeData;
+  private final Color color;
 
-    public TreeType(String name, Color color, String otherTreeData) {
-        this.name = name;
-        this.color = color;
-        this.otherTreeData = otherTreeData;
-    }
+  public TreeType(Color color) {
+    this.color = color;
+  }
 
-    public void draw(Graphics g, int x, int y) {
-        g.setColor(Color.BLACK);
-        g.fillRect(x - 1, y, 3, 5);
-        g.setColor(color);
-        g.fillOval(x - 5, y - 10, 10, 10);
-    }
+  public void draw(Graphics g, int x, int y) {
+    g.setColor(Color.BLACK);
+    g.fillRect(x - 1, y, 3, 5);
+    g.setColor(color);
+    g.fillOval(x - 5, y - 10, 10, 10);
+  }
 }
