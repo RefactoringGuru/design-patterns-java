54520eed916d7c39e57ef1d7d79cef12e75b973d
Kalin Ivanov <kalin2578@gmail.com>
2023-10-04 11:25:15 +0300

    some changes in the chain of responsibility module

--- src/refactoring_guru/chain_of_responsibility/example/Demo.java
+++ src/refactoring_guru/chain_of_responsibility/example/Demo.java
@@ -9,48 +9,52 @@ import refactoring_guru.chain_of_responsibility.example.server.Server;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Optional;
 
 /**
  * EN: Demo class. Everything comes together here.
- *
+ * <p>
  * RU: Демо-класс. Здесь всё сводится воедино.
  */
 public class Demo {
-    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-    private static Server server;
+  private static Server server;
 
-    private static void init() {
-        server = new Server();
-        server.register("admin@example.com", "admin_pass");
-        server.register("user@example.com", "user_pass");
+  public static void main(String[] args) {
+    init();
+    while(!attemptLogin()) {
+      System.out.println("Error reading input. Try again.");
+    }
+  }
 
-        // EN: All checks are linked. Client can build various chains using the
-        // same components.
-        //
-        // RU: Проверки связаны в одну цепь. Клиент может строить различные
-        // цепи, используя одни и те же компоненты.
-        Middleware middleware = Middleware.link(
+  private static void init() {
+    server = new Server();
+    server.register("admin@example.com", "admin_pass");
+    server.register("user@example.com", "user_pass");
+
+    Middleware middleware = Middleware.link(
             new ThrottlingMiddleware(2),
             new UserExistsMiddleware(server),
             new RoleCheckMiddleware()
-        );
+    );
 
-        // EN: Server gets a chain from client code.
-        //
-        // RU: Сервер получает цепочку от клиентского кода.
-        server.setMiddleware(middleware);
+    server.setMiddleware(middleware);
+  }
+
+  private static boolean attemptLogin() {
+    return Optional.ofNullable(getCredentials())
+            .map(credentials -> server.logIn(credentials.email(), credentials.password()))
+            .orElse(false);
+  }
+
+  private static Credentials getCredentials() {
+    try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+      System.out.print("Enter email: ");
+      String email = reader.readLine();
+      System.out.print("Input password: ");
+      String password = reader.readLine();
+      return new Credentials(email, password);
+    } catch (IOException e) {
+      return null;
     }
-
-    public static void main(String[] args) throws IOException {
-        init();
-
-        boolean success;
-        do {
-            System.out.print("Enter email: ");
-            String email = reader.readLine();
-            System.out.print("Input password: ");
-            String password = reader.readLine();
-            success = server.logIn(email, password);
-        } while (!success);
-    }
+  }
 }
--- src/refactoring_guru/chain_of_responsibility/example/middleware/UserExistsMiddleware.java
+++ src/refactoring_guru/chain_of_responsibility/example/middleware/UserExistsMiddleware.java
@@ -4,25 +4,25 @@ import refactoring_guru.chain_of_responsibility.example.server.Server;
 
 /**
  * EN: ConcreteHandler. Checks whether a user with the given credentials exists.
- *
+ * <p>
  * RU: Конкретный элемент цепи обрабатывает запрос по-своему.
  */
 public class UserExistsMiddleware extends Middleware {
-    private Server server;
+  private final Server server;
 
-    public UserExistsMiddleware(Server server) {
-        this.server = server;
-    }
+  public UserExistsMiddleware(Server server) {
+    this.server = server;
+  }
 
-    public boolean check(String email, String password) {
-        if (!server.hasEmail(email)) {
-            System.out.println("This email is not registered!");
-            return false;
-        }
-        if (!server.isValidPassword(email, password)) {
-            System.out.println("Wrong password!");
-            return false;
-        }
-        return checkNext(email, password);
+  public boolean check(String email, String password) {
+    if (!server.hasEmail(email)) {
+      System.out.println("This email is not registered!");
+      return false;
     }
+    if (!server.isValidPassword(email, password)) {
+      System.out.println("Wrong password!");
+      return false;
+    }
+    return checkNext(email, password);
+  }
 }
