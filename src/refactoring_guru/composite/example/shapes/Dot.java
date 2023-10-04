da93f163f13ed2c3d25c3713941c3cf3fb3ea002
Kalin Ivanov <kalin2578@gmail.com>
2023-10-04 13:55:57 +0300

    some changes in the composite package

--- src/refactoring_guru/composite/example/shapes/Dot.java
+++ src/refactoring_guru/composite/example/shapes/Dot.java
@@ -3,25 +3,24 @@ package refactoring_guru.composite.example.shapes;
 import java.awt.*;
 
 public class Dot extends BaseShape {
-    private final int DOT_SIZE = 3;
 
-    public Dot(int x, int y, Color color) {
-        super(x, y, color);
-    }
+  public Dot(int x, int y, Color color) {
+    super(x, y, color);
+  }
 
-    @Override
-    public int getWidth() {
-        return DOT_SIZE;
-    }
+  @Override
+  public int getWidth() {
+    return 3;
+  }
 
-    @Override
-    public int getHeight() {
-        return DOT_SIZE;
-    }
+  @Override
+  public int getHeight() {
+    return 3;
+  }
 
-    @Override
-    public void paint(Graphics graphics) {
-        super.paint(graphics);
-        graphics.fillRect(x - 1, y - 1, getWidth(), getHeight());
-    }
+  @Override
+  public void paint(Graphics graphics) {
+    super.paint(graphics);
+    graphics.fillRect(x - 1, y - 1, getWidth(), getHeight());
+  }
 }
