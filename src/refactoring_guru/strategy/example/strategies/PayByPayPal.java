5e6bac44d68b8920b5613229b3d3b75d997fe48b
Kalin Ivanov <kalin2578@gmail.com>
2023-10-05 18:08:12 +0300

    some changes in Strategy package

--- src/refactoring_guru/strategy/example/strategies/PayByPayPal.java
+++ src/refactoring_guru/strategy/example/strategies/PayByPayPal.java
@@ -8,69 +8,69 @@ import java.util.Map;
 
 /**
  * EN: Concrete strategy. Implements PayPal payment method.
- *
+ * <p>
  * RU: Конкретная стратегия. Реализует оплату корзины интернет магазина через
  * платежную систему PayPal.
  */
 public class PayByPayPal implements PayStrategy {
-    private static final Map<String, String> DATA_BASE = new HashMap<>();
-    private final BufferedReader READER = new BufferedReader(new InputStreamReader(System.in));
-    private String email;
-    private String password;
-    private boolean signedIn;
+  private static final Map<String, String> DATA_BASE = new HashMap<>();
+  private final BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+  private String email;
+  private String password;
+  private boolean signedIn;
 
-    static {
-        DATA_BASE.put("amanda1985", "amanda@ya.com");
-        DATA_BASE.put("qwerty", "john@amazon.eu");
-    }
+  static {
+    DATA_BASE.put("amanda1985", "amanda@ya.com");
+    DATA_BASE.put("qwerty", "john@amazon.eu");
+  }
 
-    /**
-     * EN: Collect customer's data.
-     *
-     * RU: Собираем данные от клиента.
-     */
-    @Override
-    public void collectPaymentDetails() {
-        try {
-            while (!signedIn) {
-                System.out.print("Enter the user's email: ");
-                email = READER.readLine();
-                System.out.print("Enter the password: ");
-                password = READER.readLine();
-                if (verify()) {
-                    System.out.println("Data verification has been successful.");
-                } else {
-                    System.out.println("Wrong email or password!");
-                }
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    private boolean verify() {
-        setSignedIn(email.equals(DATA_BASE.get(password)));
-        return signedIn;
-    }
-
-    /**
-     * EN: Save customer data for future shopping attempts.
-     *
-     * RU: Если клиент уже вошел в систему, то для следующей оплаты данные
-     * вводить не придется.
-     */
-    @Override
-    public boolean pay(int paymentAmount) {
-        if (signedIn) {
-            System.out.println("Paying " + paymentAmount + " using PayPal.");
-            return true;
+  /**
+   * EN: Collect customer's data.
+   * <p>
+   * RU: Собираем данные от клиента.
+   */
+  @Override
+  public void collectPaymentDetails() {
+    try {
+      while (!signedIn) {
+        System.out.print("Enter the user's email: ");
+        email = bufferedReader.readLine();
+        System.out.print("Enter the password: ");
+        password = bufferedReader.readLine();
+        if (verify()) {
+          System.out.println("Data verification has been successful.");
         } else {
-            return false;
+          System.out.println("Wrong email or password!");
         }
+      }
+    } catch (IOException ex) {
+      ex.printStackTrace();
     }
+  }
 
-    private void setSignedIn(boolean signedIn) {
-        this.signedIn = signedIn;
+  /**
+   * EN: Save customer data for future shopping attempts.
+   * <p>
+   * RU: Если клиент уже вошел в систему, то для следующей оплаты данные
+   * вводить не придется.
+   */
+  @Override
+  public boolean pay(int paymentAmount) {
+    if (signedIn) {
+      System.out.println("Paying " + paymentAmount + " using PayPal.");
+      return true;
+    } else {
+      return false;
     }
+  }
+
+  private boolean verify() {
+    setSignedIn(email.equals(DATA_BASE.get(password)));
+    return signedIn;
+  }
+
+  private void setSignedIn(boolean signedIn) {
+    this.signedIn = signedIn;
+  }
 }
 
--- src/refactoring_guru/strategy/example/strategies/PayStrategy.java
+++ src/refactoring_guru/strategy/example/strategies/PayStrategy.java
@@ -2,11 +2,12 @@ package refactoring_guru.strategy.example.strategies;
 
 /**
  * EN: Common interface for all strategies.
- *
+ * <p>
  * RU: Общий интерфейс всех стратегий.
  */
 public interface PayStrategy {
-    boolean pay(int paymentAmount);
-    void collectPaymentDetails();
+  boolean pay(int paymentAmount);
+
+  void collectPaymentDetails();
 }
 
