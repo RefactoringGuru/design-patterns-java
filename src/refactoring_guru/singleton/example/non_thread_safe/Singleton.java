73cfa09e412eba066da325b76cedc05812248a09
Kalin Ivanov <kalin2578@gmail.com>
2023-10-05 15:58:43 +0300

    some changes in Singleton package

--- src/refactoring_guru/singleton/example/non_thread_safe/Singleton.java
+++ src/refactoring_guru/singleton/example/non_thread_safe/Singleton.java
@@ -1,25 +1,29 @@
 package refactoring_guru.singleton.example.non_thread_safe;
 
 public final class Singleton {
-    private static Singleton instance;
-    public String value;
+  private static Singleton instance;
+  public String value;
 
-    private Singleton(String value) {
-        // EN: The following code emulates slow initialization.
-        //
-        // RU: Этот код эмулирует медленную инициализацию.
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException ex) {
-            ex.printStackTrace();
-        }
-        this.value = value;
+  /**
+   * EN: The following code emulates slow initialization.
+   * <p>
+   * RU: Этот код эмулирует медленную инициализацию.
+   *
+   * @param value example value
+   */
+  private Singleton(String value) {
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException ex) {
+      Thread.currentThread().interrupt();
     }
+    this.value = value;
+  }
 
-    public static Singleton getInstance(String value) {
-        if (instance == null) {
-            instance = new Singleton(value);
-        }
-        return instance;
+  public static Singleton getInstance(String value) {
+    if (instance == null) {
+      instance = new Singleton(value);
     }
+    return instance;
+  }
 }
