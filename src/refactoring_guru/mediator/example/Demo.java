2e022d93d51b9df9abfb8df2697cc470ced9b455
Kalin Ivanov <kalin2578@gmail.com>
2023-10-05 15:43:07 +0300

    some changes in Mediator package

--- src/refactoring_guru/mediator/example/Demo.java
+++ src/refactoring_guru/mediator/example/Demo.java
@@ -3,26 +3,38 @@ package refactoring_guru.mediator.example;
 import refactoring_guru.mediator.example.components.*;
 import refactoring_guru.mediator.example.mediator.Editor;
 import refactoring_guru.mediator.example.mediator.Mediator;
+import refactoring_guru.mediator.example.mediator.Note;
 
 import javax.swing.*;
 
 /**
  * EN: Demo class. Everything comes together here.
- *
+ * <p>
  * RU: Демо-класс. Здесь всё сводится воедино.
  */
 public class Demo {
-    public static void main(String[] args) {
-        Mediator mediator = new Editor();
+  public static void main(String[] args) {
+    Mediator mediator = new Editor();
+    ListModel<Note> noteList = new AbstractListModel<>() {
+      @Override
+      public int getSize() {
+        return 0;
+      }
 
-        mediator.registerComponent(new Title());
-        mediator.registerComponent(new TextBox());
-        mediator.registerComponent(new AddButton());
-        mediator.registerComponent(new DeleteButton());
-        mediator.registerComponent(new SaveButton());
-        mediator.registerComponent(new List(new DefaultListModel()));
-        mediator.registerComponent(new Filter());
+      @Override
+      public Note getElementAt(int i) {
+        return null;
+      }
+    };
 
-        mediator.createGUI();
-    }
+    mediator.registerComponent(new Title());
+    mediator.registerComponent(new TextBox());
+    mediator.registerComponent(new AddButton());
+    mediator.registerComponent(new DeleteButton());
+    mediator.registerComponent(new SaveButton());
+    mediator.registerComponent(new InitList(new DefaultListModel<>()));
+    mediator.registerComponent(new Filter(mediator, noteList));
+
+    mediator.createGUI();
+  }
 }
--- src/refactoring_guru/mediator/example/components/Filter.java
+++ src/refactoring_guru/mediator/example/components/Filter.java
@@ -5,61 +5,57 @@ import refactoring_guru.mediator.example.mediator.Note;
 
 import javax.swing.*;
 import java.awt.event.KeyEvent;
-import java.util.ArrayList;
+import javax.swing.DefaultListModel;
 
 /**
  * EN: Concrete components don't talk with each other. They have only one
  * communication channel–sending requests to the mediator.
- *
+ * <p>
  * RU: Конкретные компоненты никак не связаны между собой. У них есть только
  * один канал общения – через отправку уведомлений посреднику.
  */
 public class Filter extends JTextField implements Component {
-    private Mediator mediator;
-    private ListModel listModel;
+  private transient Mediator mediator;
+  private transient ListModel<Note> listModel;
 
-    public Filter() {}
+  public Filter(Mediator mediator, ListModel<Note> listModel) {
+    this.mediator = mediator;
+    this.listModel = listModel;
+  }
 
-    @Override
-    public void setMediator(Mediator mediator) {
-        this.mediator = mediator;
+  @Override
+  public void setMediator(Mediator mediator) {
+    this.mediator = mediator;
+  }
+
+  @Override
+  protected void processComponentKeyEvent(KeyEvent keyEvent) {
+    String start = getText();
+    searchElements(start);
+  }
+
+  public void setList(ListModel<Note> listModel) {
+    this.listModel = listModel;
+  }
+
+  private void searchElements(String s) {
+    if (listModel == null || s.equals("")) {
+      mediator.setElementsList(listModel);
+      return;
     }
 
-    @Override
-    protected void processComponentKeyEvent(KeyEvent keyEvent) {
-        String start = getText();
-        searchElements(start);
+    DefaultListModel<Note> filteredListModel = new DefaultListModel<>();
+    for (int i = 0; i < listModel.getSize(); i++) {
+      Note note = listModel.getElementAt(i);
+      if (note.getName().contains(s)) {
+        filteredListModel.addElement(note);
+      }
     }
+    mediator.setElementsList(filteredListModel);
+  }
 
-    public void setList(ListModel listModel) {
-        this.listModel = listModel;
-    }
-
-    private void searchElements(String s) {
-        if (listModel == null) {
-            return;
-        }
-
-        if (s.equals("")) {
-            mediator.setElementsList(listModel);
-            return;
-        }
-
-        ArrayList<Note> notes = new ArrayList<>();
-        for (int i = 0; i < listModel.getSize(); i++) {
-            notes.add((Note) listModel.getElementAt(i));
-        }
-        DefaultListModel<Note> listModel = new DefaultListModel<>();
-        for (Note note : notes) {
-            if (note.getName().contains(s)) {
-                listModel.addElement(note);
-            }
-        }
-        mediator.setElementsList(listModel);
-    }
-
-    @Override
-    public String getName() {
-        return "Filter";
-    }
+  @Override
+  public String getName() {
+    return "Filter";
+  }
 }
