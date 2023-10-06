d24b272938e4205af55d27a18cb631e45d0a8234
Kalin Ivanov <kalin2578@gmail.com>
2023-10-06 22:11:58 +0300

    some changes in visitor package

--- src/refactoring_guru/visitor/example/shapes/Rectangle.java
+++ src/refactoring_guru/visitor/example/shapes/Rectangle.java
@@ -3,52 +3,52 @@ package refactoring_guru.visitor.example.shapes;
 import refactoring_guru.visitor.example.visitor.Visitor;
 
 public class Rectangle implements Shape {
-    private int id;
-    private int x;
-    private int y;
-    private int width;
-    private int height;
+  private final int id;
+  private final int x;
+  private final int y;
+  private final int width;
+  private final int height;
 
-    public Rectangle(int id, int x, int y, int width, int height) {
-        this.id = id;
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-    }
+  public Rectangle(int id, int x, int y, int width, int height) {
+    this.id = id;
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+  }
 
-    @Override
-    public String accept(Visitor visitor) {
-        return visitor.visitRectangle(this);
-    }
+  @Override
+  public String accept(Visitor visitor) {
+    return visitor.visitRectangle(this);
+  }
 
-    @Override
-    public void move(int x, int y) {
-        // move shape
-    }
+  @Override
+  public void move(int x, int y) {
+    // move shape
+  }
 
-    @Override
-    public void draw() {
-        // draw shape
-    }
+  @Override
+  public void draw() {
+    // draw shape
+  }
 
-    public int getId() {
-        return id;
-    }
+  public int getId() {
+    return id;
+  }
 
-    public int getX() {
-        return x;
-    }
+  public int getX() {
+    return x;
+  }
 
-    public int getY() {
-        return y;
-    }
+  public int getY() {
+    return y;
+  }
 
-    public int getWidth() {
-        return width;
-    }
+  public int getWidth() {
+    return width;
+  }
 
-    public int getHeight() {
-        return height;
-    }
+  public int getHeight() {
+    return height;
+  }
 }
